fceab6db8282ed494bcc61ca54fad79b6749d757
Aditi-isra <17833501+Aditi-isra@users.noreply.github.com>
2021-02-20 18:55:53 -0600

    Update app_run2run.sh

--- test/app_run2run.sh
+++ test/app_run2run.sh
@@ -16,55 +16,56 @@ declare -a apps=("firestarter" "stream" "dgemm") #any name
 declare -a appParams=(" -t 300" " -s 655360000 -n 1500 > results/BSRUN_OUT" " 72 > results/DGEMMRUN_OUT") # ex
 '
 declare -a progs=("BFS") #executable name
-declare -a apps=("bfsfile") #any name
-declare -a appParams=(" --device 1 -s 655360000 -n 1500 > results/BFS" ) # ex
+#declare -a apps=("bfsfile") #any name
+declare -a appParams=(" > results/BFS_RUN_OUTPUT" ) # ex
 
 nRuns=$((numRuns-1))
-for i in $(seq 0 $nRuns)
+for i in $(seq 0 $nRuns) # 3*1*167 files = 501 output files 
 do
     appID=0
-    for app in "${apps[@]}"
+    for prog in "${progs[@]}"
     do
 	# DVFS
-	while IFS= read -r line 
-	do 
+	while IFS= read -r line
+	do
 	    IFS=':' read -ra l <<< "$line"
 	    s=$(echo ${l[0]} | tr -d ' ')
-	    if [[ $s == "Graphics" ]] 
-	    then 
-		IFS=' ' read -ra ll <<< ${l[1]} 
+	    if [[ $s == "Graphics" ]]
+	    then
+		IFS=' ' read -ra ll <<< ${l[1]}       #reading parameter into ll
 
 		 # GET RESULTS WITH NEW FREQUENCY
 
-		echo "### $app - ${ll[0]} (MHz) - Iteration: $i ###"
-	
+		echo "### $prog - ${ll[0]} (MHz) - Iteration: $i ###"
+
 		curPwrLimit=250
-		freq=${ll[0]}
-		file="results/$app-$prec-$mode-$curPwrLimit-$arch-$freq-"
-		
+		freq=${ll[0]} #reading freq dynamically
+		file="results/$prog-$prec-$mode-$curPwrLimit-$arch-$freq-"  #mode - run and load (loadscaling) ,curPwrLimit= max power limit of gpu
+
 		pushd ..
 		base=$(pwd)/
 		popd
-		pushd apps
-		appPath=$(pwd)/
+		pushd progs
+		progPath=$(pwd)/
 		popd
-                
-		appCmd="$curPwrLimit $freq $base $appPath${progs[$appID]}${appParams[$appID]}"
+
+		appCmd="$curPwrLimit $freq $base $progPath${progs[$appID]}${appParams[$appID]}" # appCMD is passed as argument to control ,
 		#echo $appCmd
-		
+
 		../utility/control $appCmd
 		mv ../utility/$oldfile $file$i
-		
+
 		# SAVE HBM BW for STREAM
-		if [[ $app == "stream" ]]
+		if [[ $prog == "BFS" ]]
 		then
-		    r=$(grep Triad results/BSRUN_OUT)
-		    echo $r | cut -d" " -f 2 >> results/run_triads_dvfs_$freq
+		  #  r=$(grep Triad results/BFS_RUN_OUTPUT) # instead of TRIAD write the parameter we need to grep --uncomment 
+		  #  echo $r | cut -d" " -f 2 >> results/run_triads_dvfs_$freq # cut floating point to 2 decimal
+		  
 		fi
-		
+: '
 		# SAVE GFLOPS for DGEMM
 		if [[ $app == "dgemm" ]]
-                then                                                                                             
+                then
                     r=$(grep Performance results/DGEMMRUN_OUT)
 		    echo $r
 		    IFS=',' read -ra DGEMM <<< "$r"
@@ -72,35 +73,35 @@ do
 		    IFS=' ' read -ra TIMEMS <<< "${DGEMM[1]}"
 		    echo "${FLOPS[1]} ${TIMEMS[1]}" >> results/run_perf_dvfs_$freq
 		fi
-		
-	    fi 
+'
+	    fi
 	done <"temp" # loop through supported frequency ranges
-	
+
 	# POWER Cap: 125 to 250 @ 5W increment
 	curPwrLimit=125 # read from the supported lower power limits
 	while [ $curPwrLimit -le 250 ] # read from the supported upper power limits
 	do
 	    # GET RESULTS WITH NEW POWER LIMIT
-	    echo "### $app - $curPwrLimit (W) - Iteration: $i ###"
+	    echo "### $prog - $curPwrLimit (W) - Iteration: $i ###"
 
 	    freq=1380 # read max from the supported clocks
-	    file="results/$app-$prec-$mode-$curPwrLimit-$arch-$freq-"
-	    
+	    file="results/$prog-$prec-$mode-$curPwrLimit-$arch-$freq-"
+
             pushd ..
             base=$(pwd)/
             popd
-            pushd apps
-            appPath=$(pwd)/
+            pushd progs
+            progPath=$(pwd)/
             popd
 
-            appCmd="$curPwrLimit $freq $base $appPath${progs[$appID]}${appParams[$appID]}"
+            appCmd="$curPwrLimit $freq $base $progPath${progs[$appID]}${appParams[$appID]}"
             #echo $appCmd
-            
+
 	    ../utility/control $appCmd
 	    mv ../utility/$oldfile $file$i
-	    
-	    #results/BSRUN_OUT_$i
 
+	    #results/BSRUN_OUT_$i
+: '
 	    if [[ $app == "dgemm" ]]
                 then
                     r=$(grep Performance results/DGEMMRUN_OUT)
@@ -110,19 +111,19 @@ do
                     IFS=' ' read -ra TIMEMS <<< "${DGEMM[1]}"
 		    echo "${FLOPS[1]} ${TIMEMS[1]}" >> results/run_perf_pl_$curPwrLimit
             fi
-	    
+'
 	    # SAVE HBM BW for STREAM
             if [[ $app == "stream" ]]
             then
                 r=$(grep Triad results/BSRUN_OUT)
                 echo $r | cut -d" " -f 2 >> results/run_triads_pl_$curPwrLimit
 	    fi
-	    
+
 	    #update the power limit
 	    curPwrLimit=$((curPwrLimit+5))
 	done
 	appID=$((appID+1))
-   done #apps
+   done #progs
 
 done #runs
 
@@ -151,7 +152,6 @@ do
    mv ../utility/$oldfile $file$i
    echo "Run->$app$i"
 done
-
 echo "#####################################################BABEL STREAM#########################################"
 app="stream"
 prec="fp64"
@@ -163,7 +163,6 @@ pstate=P0
 file="test_STREAM/results/$app-$prec-$mode-$pwrlimit-$arch-$pstate-"
 oldfile="changeme"
 prog=cuda-stream
-
 for i in $(seq 0 $numRuns)
 do
    pushd ..
@@ -178,10 +177,7 @@ do
    echo $r | cut -d" " -f 2 >> $appPath/results/runtriads_$pwrlimit
    echo "Run->$app$i"
 done
-
-
 ##############################CUBLAS DGEMM###########################################
-
 prec="fp64"
 app="dgemm"
 mode="run"
@@ -202,14 +198,12 @@ do
    popd
    ../utility/control $pwrlimit $pstate $base $appPath$prog 72 > $appPath/results/DGEMMRUN_OUT_$i
    mv ../utility/$oldfile $file$i
-
    r=$(grep Performance $appPath/results/DGEMMRUN_OUT_$i)
    echo $r
    IFS=',' read -ra DGEMM <<< "$r"
    IFS=' ' read -ra FLOPS <<< "${DGEMM[0]}"
    IFS=' ' read -ra TIMEMS <<< "${DGEMM[1]}"
    echo "${FLOPS[1]} ${TIMEMS[1]}" >> $appPath/results/runperf_$pstate
-
    echo "Run->$app$i"
 done
 '
